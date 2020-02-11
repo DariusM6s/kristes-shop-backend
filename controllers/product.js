930ec7e516cfe2d5a6887c7804ed2ad488ee5228
DariusM6s <dariusm6s@outlook.com>
2020-02-11 11:31:03 +0000

    add route for products search

--- controllers/product.js
+++ controllers/product.js
@@ -229,3 +229,25 @@ exports.photo = (req, res, next) => {
 	}
 	next();
 };
+
+exports.listSearch = (req, res) => {
+	// Create query object to hold serach and category values
+	const query = {};
+	// assing search value to query.name
+	if (req.query.search) {
+		query.name = { $regex: req.query.search, $options: 'i' };
+		// assign category value to query.category
+		if (req.query.category && req.query.category != 'All') {
+			query.category = req.query.category;
+		}
+		// find the product based on query object with 2 properties search & category
+		Product.find(query, (err, products) => {
+			if (err) {
+				return res.status(400).json({
+					error : errorHandler(err)
+				});
+			}
+			res.json(products);
+		}).select('-photo');
+	}
+};
--- routes/product.js
+++ routes/product.js
@@ -11,7 +11,8 @@ const {
 	listRelated,
 	listCategories,
 	listBySearch,
-	photo
+	photo,
+	listSearch
 } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
@@ -22,6 +23,7 @@ router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, rem
 router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
 
 router.get('/products', list);
+router.get('/products/search', listSearch);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
 router.post('/products/by/search', listBySearch);
